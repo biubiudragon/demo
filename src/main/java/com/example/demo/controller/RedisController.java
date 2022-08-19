479e845ecced51b29eaba0a0abd55d2de543bdae
biubiudragon <719172046@qq.com>
2022-08-19 09:09:58 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/RedisController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.UserEntity;
+import com.example.demo.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+
+@RequestMapping("/redis")
+@RestController
+@Slf4j
+public class RedisController {
+
+    private static int ExpireTime = 60;   // redis中存储的过期时间60s
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @RequestMapping("/set")
+    public boolean redisset(String key, String value){
+
+        UserEntity userEntity =new UserEntity();
+        userEntity.setId(Long.valueOf(1));
+        userEntity.setGuid(String.valueOf(1));
+        userEntity.setName("zhangsan");
+        userEntity.setAge(String.valueOf(20));
+        userEntity.setCreateTime(new Date());
+
+        //return redisUtil.set(key,userEntity,ExpireTime);
+
+        return redisUtil.set(key,value);
+    }
+
+    @RequestMapping("/get")
+    public Object redisget(String key){
+        return redisUtil.get(key);
+    }
+
+    @RequestMapping("expire")
+    public boolean expire(String key){
+        return redisUtil.expire(key,ExpireTime);
+    }
+
+}
---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+@Data
+public class UserEntity implements Serializable {
+
+    private Long id;
+    private String guid;
+    private String name;
+    private String age;
+    private Date createTime;
+
+
+}
